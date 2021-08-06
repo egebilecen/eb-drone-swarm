6f4045c083e8fdb0df63295b935c19dabefdc9ca
Ege Bilecen <egebilecen@icloud.com>
2021-08-06 12:57:37 +0300

    added function for getting angle between two pointa

--- js/math.js
+++ js/math.js
@@ -1,12 +1,34 @@
+function map_value(val, in_min, in_max, out_min, out_max)
+{
+    if     (val < in_min) return out_min;
+    else if(val > in_max) return out_max;
+
+    return (val - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
+
 function rad_to_deg(rad_val)
 {
     return rad_val * (180 / Math.PI);
 }
 
-function angle_of(pt1, pt2)
+function compass_angle_of(pt1, pt2)
 {
     let deltaX = pt2.x - pt1.x;
     let deltaY = pt2.y - pt1.y;
+    
+    let deg    = rad_to_deg(Math.atan2(deltaY, deltaX));
 
-    return rad_to_deg(Math.atan2(deltaY, deltaX));
+    return (map_value(deg, -180, 180, 0, 360) + 270) % 360;
+}
+
+function angle_of(pt1, pt2, deg_360=true)
+{
+    let deltaX = pt2.x - pt1.x;
+    let deltaY = pt2.y - pt1.y;
+    
+    let deg    = rad_to_deg(Math.atan2(deltaY, deltaX));
+
+    if(deg_360) return map_value(deg, -180, 180, 0, 360);
+
+    return deg;
 }
