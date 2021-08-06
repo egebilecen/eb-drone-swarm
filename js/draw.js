6fdc4c0d165c8416a72d2d6d08b9b5975416f84c
Ege Bilecen <egebilecen@icloud.com>
2021-08-06 11:53:21 +0300

    colored path lines

--- js/draw.js
+++ js/draw.js
@@ -4,6 +4,7 @@ function draw_drone(ctx, drone)
     ctx.arc(drone.pos.x, drone.pos.y, drone.size, 0, 2 * Math.PI, false);
     ctx.fillStyle = drone.color.background;
     ctx.fill();
+    
     ctx.lineWidth = drone.border_size;
     ctx.strokeStyle = drone.color.border;
     ctx.stroke();
@@ -16,13 +17,18 @@ function draw_drone_path_line(ctx, drone)
     ctx.moveTo(drone.pos.x,  drone.pos.y);
     ctx.lineTo(drone.pos.dest.x, drone.pos.dest.y);
     ctx.lineWidth = 3;
+    ctx.strokeStyle = drone.color.background;
     ctx.stroke();
     ctx.closePath();
 
     ctx.beginPath();
     ctx.arc(drone.pos.dest.x, drone.pos.dest.y, 6, 0, 2 * Math.PI, false);
-    ctx.fillStyle = "white";
+    ctx.fillStyle = drone.color.background;
     ctx.fill();
+
+    ctx.lineWidth = drone.border_size;
+    ctx.strokeStyle = drone.color.border;
+    ctx.stroke();
     ctx.closePath();
 }
 
